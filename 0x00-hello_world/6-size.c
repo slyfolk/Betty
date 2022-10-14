d16137c3107446b837bccad5addbe3130f699828
slyfolk <sylvester.ekenma123@gmail.com>
2022-10-14 09:08:26 -0700

    6-size.c checkers all green

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint for the program
+ *
+ * Description: the function prints the size of
+ * various datatypes as stringed integers using
+ * the sizeof() function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -5,3 +5,4 @@ Scripts for C processes:
 3-name
 4-puts.c
 5-printf.c
+6-size.c
