d34c32741cc3d405da007a551933aadf6c7e8f51
Sylvester Ekenma <30693486+slyfolk@users.noreply.github.com>
2022-10-15 00:05:51 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
 * main - print if the number is positive, zero, or negative
-    *
+*
 * Description: using the main function
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
@@ -30,4 +30,3 @@ else if (n < 0)
 }
 return (0);
 }
-
