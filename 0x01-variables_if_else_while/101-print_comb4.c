db38c19f76525ae453eaab1a1d831414ef0bb862
slyfolk <sylvester.ekenma123@gmail.com>
2022-10-15 13:12:13 -0700

    101-print_comb4.c betty green

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
-* main - print all combinations of three different digits
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+ * main - print all combinations of three different digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) /*main entrypoint*/
 {
 	int one;
 	int ten;
 	int hundred;
 
-	for (hundred = '0'; hundred <= '9'; hundred++) /* hundreds place*/
+	for (hundred = '0'; hundred <= '9'; hundred++)
 	{
-		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+		for (ten = (hundred + 1); ten <= '9'; ten++) /* hundreds place*/
 		{
-			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			for (one = (ten + 1); one <= '9'; one++) /*tens=100s+1*/
 			{
 				putchar(hundred);
 				putchar(ten);
 				putchar(one);
-				if (hundred != '7' || ten != '8' || one != '9')
+				if (hundred != '7' || ten != '8' || one != '9') /*ones*/
 				{
 					putchar(',');
 					putchar(' ');
@@ -29,6 +29,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
