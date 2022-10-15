c397ebeae194b8f8a58201069d2c023913172681
slyfolk <sylvester.ekenma123@gmail.com>
2022-10-15 01:23:15 -0700

    draining tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,28 +6,20 @@
  * main - prints sign
  *
  * Description: indicates positive or negative
- *
- * Return: 0 at the end
+ * Return: 0
  */
-
-
 int main(void)
-
 {
-        int n;
-
-        srand(time(0));
-
-        n = rand() - RAND_MAX / 2;
-
-
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
-        else
-                printf("%d is negative\n", n);
-
-
-        return (0);
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 C learning codes
 0-positive_or_negative
+1-last_digit.c
