e499fbb08e0b2e6afce7ac3d1a7252062851dca7
slyfolk <sylvester.ekenma123@gmail.com>
2022-10-16 13:45:27 -0700

    101-quote.c betty green

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
-* Description: 'the program's description'
-* Return: Always 0(Success)
-*/
+ * main - Entry point
+ *
+ * Description: prints exactly and that piece of art is useful
+ * Return: 1
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
